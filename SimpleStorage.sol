3e228f45f307460ee99337b7c521e4d446aa66de
mishoko <mail.mihail@protonmail.com>
2024-07-31 09:47:03 +0300

    added mapping and a custom type

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,6 +3,12 @@ pragma solidity 0.8.24;
 
 contract SimpleStorage {
     uint256 internal favouriteNumber;
+    struct Person {
+        string name;
+        uint256 favouriteNumber;
+    }
+    mapping(string  => uint256) public nameToFavouriteNumber;
+    Person[] public listOfPplWithFavNums;
 
     function retrieve() public view returns (uint256) {
         return favouriteNumber;
@@ -10,5 +16,14 @@ contract SimpleStorage {
 
     function store(uint256 _favouriteNumber) public {
         favouriteNumber = _favouriteNumber;
+    }
+
+    function addPersonToList(string memory _name, uint256 _favNumber) public {
+        listOfPplWithFavNums.push(Person(_name, _favNumber));
+        nameToFavouriteNumber[_name] = _favNumber;
+    }
+
+    function getNumberByName(string memory _name) public view returns (uint256) {
+        return nameToFavouriteNumber[_name];
     }
 }
